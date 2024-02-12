69ecd8e7ed01ddf490891129e5389ffe5be1a326
samangishamim <samangi624@gmail.com>
2024-02-12 21:32:54 +0330

    create a method name generate and test

--- src/Word.java
+++ src/Word.java
@@ -1,2 +1,35 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Word {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the word : ");
+        String word = scanner.nextLine();
+
+
+        HashMap<Character, Integer> letter = new HashMap<>();
+
+        for (char a : word.toCharArray()) {
+            letter.put(a, letter.getOrDefault(a, 0) + 1);
+        }
+        generate("", word.length(), letter);
+    }
+
+    private static void generate(String p, int r, HashMap<Character, Integer> letter) {
+        if (r == 0) {
+            System.out.println(p);
+            return;
+        }
+
+        for (char c : letter.keySet()) {
+            int count = letter.get(c);
+            if (count > 0) {
+                letter.put(c, count - 1);
+                generate(p + c, r - 1, letter);
+                letter.put(c, count);
+            }
+        }
+
+    }
 }
