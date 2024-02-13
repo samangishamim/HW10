fb25d3ee3c1c7969261945d513dfbe8a6e1f2f7d
samangishamim <samangi624@gmail.com>
2024-02-13 19:43:10 +0330

    add new treeset with a command get random character set

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,19 @@
+
+import java.util.Random;
+import java.util.TreeSet;
+
 public class Tree {
+
+    public static void main(String[] args) {
+        TreeSet<Character> set1 = getRandomCharacterSet();
+        TreeSet<Character> set2 = getRandomCharacterSet();
+        System.out.println("Set 1: " + set1);
+        System.out.println("Set 2: " + set2);
+        TreeSet<Character> unionSet = findUnion(set1, set2);
+        System.out.println("Union Set: " + unionSet);
+        TreeSet<Character> commonSet = findCommonality(set1, set2);
+        System.out.println("Common Set: " + commonSet);
+    }
+
+
 }
