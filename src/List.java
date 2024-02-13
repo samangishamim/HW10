a8892235dfa6eab378297aa78689fd51a6a250f1
samangishamim <samangi624@gmail.com>
2024-02-13 22:11:12 +0330

    create a new arraylist

--- src/List.java
+++ src/List.java
@@ -1,2 +1,15 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class List {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1, 3, 7, 4, 3, 6, 5, 8, 5, 5, 2, 9, 7, 3));
+
+        ArrayList<Integer> list1 = deletePairs(list);
+
+        System.out.println(list1);
+    }
+
+
 }
