4fd79024a5d0ad8662ac56394e9897ce2953d3ce
samangishamim <samangi624@gmail.com>
2024-02-14 13:36:37 +0330

    fix the all code and rewrite it

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -3,37 +3,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class MyHashMap<K, V> {
-    private Entry<K, V>[] buckets;
-    private int capacity;
-
-    public MyHashMap(int capacity) {
-        this.capacity = capacity;
-        this.buckets = new Entry[capacity];
-    }
-
-    public void put(K key, V value) {
-        int index = getIndex(key);
-        Entry<K, V> entry = new Entry<>(key, value);
-        if (buckets[index] == null) {
-            buckets[index] = entry;
-        } else {
-            Entry<K,V> current=buckets[index];
-            while (current.next!=null){
-                if (current.key.equals(key)){
-                    current.value=value;
-                    return;
-                }else {
-                    current=current.next;
-                }
-            }
-        }
-    }
-
-    private int getIndex(K key) {
-        return Math.abs(key.hashCode() % capacity);
-    }
-
-    private static class Entry<K, V> {
+    private class Entry<K, V> {
         private K key;
         private V value;
         private Entry<K, V> next;
@@ -41,7 +11,18 @@ public class MyHashMap<K, V> {
         public Entry(K key, V value) {
             this.key = key;
             this.value = value;
-            this.next = null;
+        }
+
+        public K getKey() {
+            return this.key;
+        }
+
+        public V getValue() {
+            return this.value;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
         }
     }
 }
