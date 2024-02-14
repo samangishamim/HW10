c63eb45692d6d7da77d5b8cf9ea737aac4913a6c
samangishamim <samangi624@gmail.com>
2024-02-14 15:41:06 +0330

    complete the put and get method

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -24,5 +24,50 @@ public class MyHashMap<K, V> {
         public void setValue(V value) {
             this.value = value;
         }
+
+        private final int SIZE = 5;
+        private Entry<K, V> table[];
+
+        public MyHashMap() {
+            table = new Entry[SIZE];
+        }
+
+        public void put(K key, V value) {
+            int hash = key.hashCode() % SIZE;//12345
+            Entry<K, V> e = table[hash];
+
+            if (e == null) {
+                table[hash] = new Entry<K, V>(key, value);
+            } else {
+                while (e.next != null) {
+                    if (e.getKey() == key) {
+                        e.setValue(value);
+                        return;
+                    }
+                    e = e.next;
+                }
+                if (e.getKey() == key) {
+                    e.setValue(value);
+                    return;
+                }
+                e.next = new Entry<K, V>(key, value);
+            }
+        }
+
+        public V get(K key) {
+            int hash = key.hashCode() % SIZE;//12345
+            Entry<K, V> e = table[hash];
+
+            if (e == null) {
+                return null;
+            }
+            while (e != null) {
+                if (e.getkey() == key) {
+                    return e.getValue();
+                }
+            e=e.next;
+            }
+            return null;
+        }
     }
 }
