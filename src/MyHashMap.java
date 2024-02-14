2861ca1fdca17cb5298d4e74a88c7fb94b80811c
samangishamim <samangi624@gmail.com>
2024-02-14 12:49:46 +0330

    add method getIndex for usage in other methods

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -1,2 +1,21 @@
-public class MyHashMap {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class MyHashMap <K , V> {
+    private Entry<K,V>[]buckets;
+    private int capacity;
+    public  MyHashMap(int capacity){
+        this.capacity=capacity;
+        this.buckets=new Entry[capacity];
+    }
+    private int getIndex(K key){
+        return  Math.abs(key.hashCode()%capacity);
+    }
+    private static class Entry<K,V>{
+        private  K key;
+        private V value;
+
+
+    }
 }
