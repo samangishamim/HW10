23358e8c5c36327c72fe79d9d21e3921101bc4bc
samangishamim <samangi624@gmail.com>
2024-02-14 16:02:12 +0330

    add tow toString for the print and complete remove method

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -25,49 +25,100 @@ public class MyHashMap<K, V> {
             this.value = value;
         }
 
-        private final int SIZE = 5;
-        private Entry<K, V> table[];
-
-        public MyHashMap() {
-            table = new Entry[SIZE];
+        @Override
+        public String toString() {
+            Entry<K, V> temp = this;
+            StringBuilder sb = new StringBuilder();
+            while (temp != null) {
+                sb.append(temp.key + "->" + temp.value + ",");
+                    temp = temp.next;
+            }
+            return sb.toString();
         }
+    }
 
-        public void put(K key, V value) {
-            int hash = key.hashCode() % SIZE;//12345
-            Entry<K, V> e = table[hash];
+    private final int SIZE = 5;
+    private Entry<K, V> table[];
 
-            if (e == null) {
-                table[hash] = new Entry<K, V>(key, value);
-            } else {
-                while (e.next != null) {
-                    if (e.getKey() == key) {
-                        e.setValue(value);
-                        return;
-                    }
-                    e = e.next;
-                }
+    public MyHashMap() {
+        table = new Entry[SIZE];
+    }
+
+    public void put(K key, V value) {
+        int hash = key.hashCode() % SIZE;//12345
+        Entry<K, V> e = table[hash];
+
+        if (e == null) {
+            table[hash] = new Entry<K, V>(key, value);
+        } else {
+            while (e.next != null) {
                 if (e.getKey() == key) {
                     e.setValue(value);
                     return;
                 }
-                e.next = new Entry<K, V>(key, value);
+                e = e.next;
             }
+            if (e.getKey() == key) {
+                e.setValue(value);
+                return;
+            }
+            e.next = new Entry<K, V>(key, value);
         }
+    }
 
-        public V get(K key) {
-            int hash = key.hashCode() % SIZE;//12345
-            Entry<K, V> e = table[hash];
+    public V get(K key) {
+        int hash = key.hashCode() % SIZE;//12345
+        Entry<K, V> e = table[hash];
 
-            if (e == null) {
-                return null;
-            }
-            while (e != null) {
-                if (e.getKey() == key) {
-                    return e.getValue();
-                }
-            e=e.next;
-            }
+        if (e == null) {
             return null;
         }
+        while (e != null) {
+            if (e.getKey() == key) {
+                return e.getValue();
+            }
+            e = e.next;
+        }
+        return null;
+    }
+
+    public Entry<K, V> remove(K key) {
+        int hash = key.hashCode() % SIZE;//12345
+        Entry<K, V> e = table[hash];
+
+        if (e == null) {
+            return null;
+        }
+
+        if (e.getKey() == key) {
+            table[hash] = e.next;
+            e.next = null;
+            return e;
+        }
+
+        Entry<K, V> prev = e;
+        e = e.next;
+
+        while (e != null) {
+            if (e.getKey() == key) {
+                prev.next = e.next;
+                e.next = null;
+                return e;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < SIZE; i++) {
+            if (table[i] != null) {
+                sb.append(i + "" + table[i] + "\n");
+            } else {
+                sb.append(i + "" + "null" + "\n");
+            }
+        }
+        return sb.toString();
     }
 }
