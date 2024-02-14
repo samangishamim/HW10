07042e717959e50ebae9b4eeb2030259561d7c31
samangishamim <samangi624@gmail.com>
2024-02-14 12:52:06 +0330

    create constructor for the class and value the fields

--- src/MyHashMap.java
+++ src/MyHashMap.java
@@ -2,20 +2,28 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
-public class MyHashMap <K , V> {
-    private Entry<K,V>[]buckets;
+public class MyHashMap<K, V> {
+    private Entry<K, V>[] buckets;
     private int capacity;
-    public  MyHashMap(int capacity){
-        this.capacity=capacity;
-        this.buckets=new Entry[capacity];
+
+    public MyHashMap(int capacity) {
+        this.capacity = capacity;
+        this.buckets = new Entry[capacity];
     }
-    private int getIndex(K key){
-        return  Math.abs(key.hashCode()%capacity);
+
+    private int getIndex(K key) {
+        return Math.abs(key.hashCode() % capacity);
     }
-    private static class Entry<K,V>{
-        private  K key;
+
+    private static class Entry<K, V> {
+        private K key;
         private V value;
+        private Entry<K, V> next;
 
-
+        public Entry(K key,V value) {
+           this.key=key;
+           this.value=value;
+           this.next=null;
+        }
     }
 }
